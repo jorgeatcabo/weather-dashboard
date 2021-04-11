916bf7ec1bc86c686224687cc68fe6cec681c0f6
El Roble <san_lucas2005@yahoo.com>
2021-04-11 08:59:23 -0600

    Code for add event listener to city buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,6 +14,7 @@ function getForecast(city) {
     
     fetch(requestUrl)
         .then(function (response) {
+            console.log(response)
         return response.json();
         })
         .then(function (data) {
@@ -25,9 +26,52 @@ function getForecast(city) {
         
         var cityNameButton=document.createElement('button')
         searchResults.append(cityNameButton)
-        cityNameButton.setAttribute("data-city",city)
+        cityNameButton.setAttribute("id",city)
         cityNameButton.setAttribute("class", "btn btn-primary btn-block")
         cityNameButton.textContent=city
+
+        $("#"+city).click(event => {  
+            getForecastButtons(event,city);
+          });
+
+       
+        
+        //Getting UV with coordinates
+        fetch("https://api.openweathermap.org/data/2.5/uvi?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid="+api)
+        .then(function (uvResponse) {
+        return uvResponse.json();
+        })
+        .then(function (uvData) {
+        $('.uv').text('UV Index: '+uvData.value) 
+        });
+
+    //     for(var i=0;i<data.length;i++){
+    //     // TODO: Loop through the data and generate your HTML
+    //     var userName=document.createElement('h3')
+    //     var userLink=document.createElement('p')
+    //     userName.textContent= data[i].login
+    //     userLink.textContent= data[i].html_url
+    //     userLink.setAttribute('href',userLink.innerHTML)
+    //     userContainer.append(userName)
+    //     userContainer.append(userLink)
+        
+    //   }
+        });
+    }
+
+function getForecastButtons(event,city) {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api+"&units="+unit;
+    
+    fetch(requestUrl)
+        .then(function (response) {
+            console.log(response)
+        return response.json();
+        })
+        .then(function (data) {
+        $('.temp').text('Temp: '+data.main.temp+String.fromCharCode(176)+' C')
+        $('.wind').text('Wind: '+data.wind.speed+' MPH')
+        $('.humidity').text('Humidity: '+data.main.humidity+' %')
+        $('.city-name').text(city)        
         
         //Getting UV with coordinates
         fetch("https://api.openweathermap.org/data/2.5/uvi?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid="+api)
