22e77c287c0fcd4553255ee8d133f79749584f4a
El Roble <san_lucas2005@yahoo.com>
2021-04-12 13:39:50 -0600

    add formatUvIndex function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,5 @@
 const api="b94db98390be2c75a7a4da963d43370b";
 const unit="metric"
-//const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+api+"&units="+unit;
 const url="https://api.openweathermap.org/data/2.5/weather?q=mazatlan"+"&appid="+api+"&units="+unit;
 var searchResults=$('.search-results')
 
@@ -23,7 +22,6 @@ function getForecast(city) {
         $('.humidity').text('Humidity: '+data.main.humidity+' %')
         let today = new Date().toISOString().slice(0, 10)
         $('.city-name').text(city+"("+today+")")
-
         
         var cityNameButton=document.createElement('button')
         searchResults.append(cityNameButton)
@@ -34,9 +32,7 @@ function getForecast(city) {
         $("#"+city).click(event => {  
             getForecastButtons(event,city);
             getForecastButtons5Days(event,city);
-          });
-
-       
+          });       
         
         //Getting UV with coordinates
         fetch("https://api.openweathermap.org/data/2.5/uvi?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid="+api)
@@ -44,7 +40,14 @@ function getForecast(city) {
         return uvResponse.json();
         })
         .then(function (uvData) {
-        $('.uv').text('UV Index: '+uvData.value) 
+
+        
+        var uvText=$('.uvText')        
+        var uvNumber=$('.uvNumber')
+        uvIndexValue=Math.round(uvData.value)
+
+        formatUvIndex(uvIndexValue,uvText,uvNumber)
+
         });
         });
     }
@@ -92,10 +95,9 @@ function getForecastButtons(event,city) {
                     var icon=data.list[days[i]].weather[0].icon;
                     var imageURL="http://openweathermap.org/img/wn/"+icon+"@2x.png";
                     $('.weather-image-'+`${i + 1}`).attr("src",imageURL);
-                    $('.day-'+`${i + 1}`+'-temp').text('Temp: '+data.list[days[i]].main.temp+String.fromCharCode(176)+' C')
-                    // $('.wind').text('Wind: '+data.wind.speed+' MPH')
-                    // $('.humidity').text('Humidity: '+data.main.humidity+' %')
-                    // $('.city-name').text(city)
+                    $('.day-'+`${i + 1}`+'-temp').text('Temp: '+data.list[days[i]].main.temp+String.fromCharCode(176)+' C')                    
+                    $('.day-'+`${i + 1}`+'-wind').text('Wind: '+data.list[days[i]].wind.speed+' MPH')
+                    $('.day-'+`${i + 1}`+'-humidity').text('Humidity: '+data.list[days[i]].main.humidity+' %')                   
                 }                 
             });
         }
@@ -109,17 +111,37 @@ function getForecastButtons(event,city) {
             })
             .then(function (data) {
                 var days=[8,16,24,32,39]
-                for(var i=0;i<5;i++){                
+                for(var i=0;i<5;i++){                          
                     $('.day-'+`${i + 1}`+'-header').text(data.list[days[i]].dt_txt.split(' ')[0])
-                    // var icon=weatherData.weather[0].icon;
-                    // var imageURL="http://openweathermap.org/img/wn/"+icon+"@2x.png";
-                    // res.write("<p>The weather is currently: "+ description +"</p>");
-                    // res.write("<h1>The temperature in "+query+" is: "+ temp + " Celcius degress.</h1>");
-                    // res.write("<img src="+imageURL+">");
-
-                    // $('.wind').text('Wind: '+data.wind.speed+' MPH')
-                    // $('.humidity').text('Humidity: '+data.main.humidity+' %')
-                    // $('.city-name').text(city)
-                }     
+                    var icon=data.list[days[i]].weather[0].icon;
+                    var imageURL="http://openweathermap.org/img/wn/"+icon+"@2x.png";
+                    $('.weather-image-'+`${i + 1}`).attr("src",imageURL);
+                    $('.day-'+`${i + 1}`+'-temp').text('Temp: '+data.list[days[i]].main.temp+String.fromCharCode(176)+' C')                    
+                    $('.day-'+`${i + 1}`+'-wind').text('Wind: '+data.list[days[i]].wind.speed+' MPH')
+                    $('.day-'+`${i + 1}`+'-humidity').text('Humidity: '+data.list[days[i]].main.humidity+' %')                   
+                }                 
             });
+
         }        
+    
+    function formatUvIndex(indexValue,IndexTagText,IndexTagNumber){
+        var uvColors=
+        [
+            [1,2,'Green'],
+            [3,5,'Yellow'],
+            [6,7,'Orange'],
+            [8,10,'Red'],
+            [11,20,'Violet']
+        ];
+
+        IndexTagText.text('UV Index: ')
+
+        for(i=0;i<uvColors.length;i++){
+            if(indexValue>=uvColors[i][0] && indexValue<=uvColors[i][1]){
+                IndexTagNumber.text(indexValue) 
+                IndexTagNumber.css("background-color", uvColors[i][2])
+            }
+        }        
+
+
+    }
